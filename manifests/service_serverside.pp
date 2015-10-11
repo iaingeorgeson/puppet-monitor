24e8c55a0d19412bc7aa774e2e5f3d30d92a0806
Iain Georgeson <iain.georgeson@kaust.edu.sa>
2015-10-11 11:14:01 +0300

    Try making Nagios-ng more tolerant of transitions
    The puppet code tries to extract (identical) Nagios command objects out
    of each individual host-service object. If they're not identical, Bad
    Things Happen. This is an attempt to make it more resilient, at the
    expense of making it less predictable.
    
    [Comment: #3518]

--- manifests/service_serverside.pp
+++ manifests/service_serverside.pp
@@ -1,6 +1,23 @@
 # This resource is realised on the nagios server. Don't use it
 # directly, use monitor::service instead.
 
+# These exported resources are created by the monitor::service type.
+# A monitor::service_serverside resource contains attributes to create
+# all the related Nagios objects - host, hostgroup, service and
+# command - as well as managing a Nagios check script if that's required.
+
+# One of these resources is created for each combination of host and
+# service. However we only need - for example - one host object per
+# host. We need to create one single host object from all the
+# individual service objects. The same applies to commands and
+# hostgroups.
+
+# So we use ensure_resource to make sure the host object is created
+# only once. The weakness with this is that ensure_resource requires
+# the resource to be identical each time it is called. This might not
+# be true if you're changing the attributes. We're going to see if we
+# can fix this with a different approach.
+
 define monitor::service_serverside (
     $command_line   = false,
     $command_name,
@@ -64,7 +81,11 @@ define monitor::service_serverside (
                                      parents         => $parents,
                      }})
 
-    ensure_resource  ('nagiosng::object::command', $command_name,
-                     {attributes => {command_name => $command_name,
-                                     command_line => $command_line,}})
+    if ! defined(Nagiosng::Object::Command[$command_name])
+    {
+        nagiosng::object::command { $command_name:
+            attributes => {command_name => $command_name,
+                           command_line => $command_line,}
+        }
+    }
 }
